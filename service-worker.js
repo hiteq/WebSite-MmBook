0a8d5b20e644a96e1563cbf7ae31d67bd6e215f6
hiteq <hiiteq@gmail.com>
2019-12-30 08:17:30 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/woohannah-mm-book.com/precache-manifest.03ef084563a53b022c2cf23442e856f5.js"
+  "/precache-manifest.83c0baf57d7b59c532887d2065f1f654.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/woohannah-mm-book.com/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });

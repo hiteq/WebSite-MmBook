934583f2c11e992c75d0dfbee5a3659dbe1e683f
Heetaek Woo <hiiteq@gmail.com>
2022-03-21 03:11:25 +0900

    contents update

--- src/utils/use-inverted-border-radius.js
+++ src/utils/use-inverted-border-radius.js
@@ -3,16 +3,8 @@ import { useEffect } from "react";
 export function useInvertedBorderRadius(radius) {
   const scaleX = useMotionValue(1);
   const scaleY = useMotionValue(1);
-  const inverted = useInvertedScale({ scaleX, scaleY });
   const borderRadius = useMotionValue(`${radius}px`);
   useEffect(() => {
-    function updateRadius() {
-      // const latestX = inverted.scaleX.get();
-      // const latestY = inverted.scaleY.get();
-      // const xRadius = latestX * radius + "px";
-      // const yRadius = latestY * radius + "px";
-      // borderRadius.set(`${xRadius} ${yRadius}`);
-    }
     // const unsubScaleX = inverted.scaleX.onChange(updateRadius);
     // const unsubScaleY = inverted.scaleY.onChange(updateRadius);
     return () => {

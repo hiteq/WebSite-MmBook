d3d4fbc7b47e3f40da98edf2cdf0550a5a3007f0
Heetaek Woo <hiiteq@gmail.com>
2022-03-20 22:16:21 +0900

    Updates

---
+++ precache-manifest.50f08c1363c289639a4a781d367c9b5d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e08f45868fcf1289b70",
+    "url": "/mm-web-book/static/css/main.e5837540.chunk.css"
+  },
+  {
+    "revision": "0e08f45868fcf1289b70",
+    "url": "/mm-web-book/static/js/main.0e08f458.chunk.js"
+  },
+  {
+    "revision": "1423a5f2c4b57dbcc672",
+    "url": "/mm-web-book/static/js/1.1423a5f2.chunk.js"
+  },
+  {
+    "revision": "4bb871d39361fde0c630",
+    "url": "/mm-web-book/static/js/runtime~main.4bb871d3.js"
+  },
+  {
+    "revision": "15255b70edd39b1c6ee209c33a027c7f",
+    "url": "/mm-web-book/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/mm-web-book/precache-manifest.c79ed811b56534bf8abe93afb0090211.js"
+  "/mm-web-book/precache-manifest.50f08c1363c289639a4a781d367c9b5d.js"
 );
 
 workbox.clientsClaim();
